4b80ee67ef73fca3aee9e33a0aced422487e33b0
Maximumdb <max.perron@eleves.ec-nantes.fr>
2026-01-10 16:22:08 +0100

    Initialisation projet

---
+++ MEDEV_TP/src/main/java/org/centrale/projet/medev_tp/MEDEV_TP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.projet.medev_tp;
+
+/**
+ *
+ * @author Max
+ */
+public class MEDEV_TP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
