928ab0e2bde6073a4f31a95babb4f067e0012890
Maximumdb <max.perron@eleves.ec-nantes.fr>
2026-01-10 18:30:06 +0100

    Initialisation projet

--- MEDEV_TP/src/test/java/org/centrale/projet/medev_tp/MEDEV_TPTest.java
+++ MEDEV_TP/src/test/java/org/centrale/projet/medev_tp/MEDEV_TPTest.java
@@ -42,10 +42,6 @@ public class MEDEV_TPTest {
     @Test
     public void testMain() {
         System.out.println("main");
-        String[] args = null;
-        MEDEV_TP.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
